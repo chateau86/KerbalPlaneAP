781a052f6695bd6938227facd07e8d136db6ea61
chateau86 <chateau86@gmail.com>
2014-05-18 11:36:13 +0700

    Preliminary groundwork

--- New_AP/KTP2/KSpG-Servo.cs
+++ New_AP/KTP2/KSpG-Servo.cs
@@ -4,11 +4,30 @@ namespace KSpG
 {
 	public class KSpG_Servo
 	{
+		public double[] TargetAtt;//{Roll,Ptch,Yaw}
+
 		public KSpG_Servo ()
-		{//UNITY HATES THIS.
+		{
+			TargetAtt= new double[3];
+		}
+			
+
+
+
+		public void SetRoll(double roll){
+			TargetAtt[0]=roll;
+		}
+		public void SetPitch(double pitch){
+			TargetAtt[1]=pitch;
+		}
+		public void SetYaw(double yaw){
+			TargetAtt[2]=yaw;
 		}
 
 
+		public void fly(){
+
+		}
 
 	}
 }
